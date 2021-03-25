cda3a108f0c61281a5de7348bdb237d801337ef3
Gdominguezborines <Gdominguezborines@danielcastelao.org>
2021-03-25 05:19:51 +0100

    creado factory

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,33 @@
 package com.company;
-
+import Libreria.*;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
+/*
+        Consola obx = new Consola();
+        obx.visualizar("Hola");
+
+        Ventana obx1 = new Ventana();
+        obx1.visualizar("Adios");
+*/
+
+
+        InterfaceMetodoComunes miSalida1,misalida2,misalida3,miEntrada1,miEntrada2;
+
+        miSalida1= FactoriaDeSalidas.getProductoSalida(FactoriaDeSalidas.CONSOLA);
+        miSalida1.visualizar("Hola que tal estas");
+
+        misalida2=FactoriaDeSalidas.getProductoSalida(FactoriaDeSalidas.VENTANA);
+        misalida2.visualizar("Bien");
+
+        misalida3=FactoriaDeSalidas.getProductoSalida(FactoriaDeSalidas.IMPRESORA);
+        misalida3.visualizar("Imprimiendo por la impresora");
+
+        miEntrada1= FactoriaDeEntradas.getProductoEntrada(FactoriaDeEntradas.CONSOLA);
+        miEntrada1.introducirDatos();
+
+        miEntrada2=FactoriaDeEntradas.getProductoEntrada(FactoriaDeEntradas.VENTANA);
+        miEntrada2.introducirDatos();
     }
 }
